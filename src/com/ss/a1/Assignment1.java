e42a704ad5037c7e6817ca313815df654a1a82d9
Jacob Fleming <jacob.fleming@smoothstack.com>
2021-11-02 18:02:01 -0400

    updated validation

--- src/com/ss/a1/Assignment1.java
+++ src/com/ss/a1/Assignment1.java
@@ -13,12 +13,16 @@ public class Assignment1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int sum=0;
-		for(String s: args) {
-			sum += Integer.parseInt(s);
-			System.out.println("running sum: "+sum);
+		int sum = 0;
+		try {
+			for (String s : args) {
+				sum += Integer.parseInt(s);
+				System.out.println("running sum: " + sum);
+			}
+			System.out.println("final sum: " + sum);
+		} catch (Exception e) {
+			System.out.println("Please only use whole numbers");
 		}
-		System.out.println("final sum: "+sum);
 	}
 
 }
